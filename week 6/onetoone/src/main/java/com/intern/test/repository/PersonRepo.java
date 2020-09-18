5b53ac0d254c57fbff2945e9b47590ce3ba0cf7d
HninyeeAye <hninyeeayetone7@gmail.com>
2020-09-18 22:31:52 +0630

    my assign for one to one

--- week 6/onetoone/src/main/java/com/intern/test/repository/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.intern.test.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.intern.test.entity.Person;
-
-public interface PersonRepo extends JpaRepository<Person, Long>  {
-
-}
